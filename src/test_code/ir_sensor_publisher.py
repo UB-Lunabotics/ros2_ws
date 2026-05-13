b009c61e1a628b94a2058b8b5b84265a5743b7f4
Ashishkabaab <ashishkbehal@gmail.com>
2026-05-13 10:51:26 -0400

    changed unit32 to float32

--- src/test_code/ir_sensor_publisher.py
+++ src/test_code/ir_sensor_publisher.py
@@ -6,7 +6,7 @@ import serial
 class IRSensorPublisher(Node):
     def __init__(self):
         super().__init__('ir_sensor_publisher')
-        self.publisher_ = self.create_publisher(UInt32, '/ir_distance', 10)
+        self.publisher_ = self.create_publisher(Float32, '/ir_sensor_value', 10)
         self.serial_port = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
         self.timer = self.create_timer(0.1, self.read_and_publish)  # 10Hz
 
